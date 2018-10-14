39fdf7ee21fafe4192a414511a28cf116eac139f
ronaldorawat <palash.rawat@gmail.com>
2018-10-14 18:56:55 +0530

    Added C pattern program

---
+++ C.java
@@ -0,0 +1,27 @@
+package AlphabetPatterns;
+
+import java.util.Scanner;
+public class C {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter the number of rows");
+		int n=scn.nextInt();
+		scn.close();
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0||j==0||i==n-1)
+				{
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+}
+}
