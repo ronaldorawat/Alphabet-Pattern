6eeb35fb31a770af030cf050db16c20b9793f664
ronaldorawat <palash.rawat@gmail.com>
2018-10-14 21:40:20 +0530

    Removed Package name from A and B patern

---
+++ Y.java
@@ -0,0 +1,27 @@
+package AlphabetPatterns;
+
+import java.util.Scanner;
+
+public class Y {
+
+	public static void main(String[] args) {
+
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter the odd number of rows");
+		int n=scn.nextInt();
+		scn.close();
+		
+		for(int i=0;i<=n;i++)
+		{	
+			for(int j=0;j<=n;j++)
+			{
+				if((i==j&&i<n/2)||i+j==n)
+				{
+					System.out.print("*");
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Z.java
@@ -0,0 +1,30 @@
+package AlphabetPatterns;
+
+import java.util.Scanner;
+
+public class Z {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter the odd number of rows");
+		int n=scn.nextInt();
+		scn.close();
+		
+		for(int i=0;i<=n;i++)
+		{	
+			for(int j=0;j<=n;j++)
+			{
+				if(i==0||i+j==n||i==n)
+				{
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+	
+}
